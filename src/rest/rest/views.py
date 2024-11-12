458246f0799a8772ebba3f5006276864f93b48e2
kunal Dugar <yoyokvunal@gmail.com>
2024-11-12 10:59:55 +0530

    Second commit

--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -5,15 +5,24 @@ from rest_framework import status
 import json, logging, os
 from pymongo import MongoClient
 
+# Set up MongoDB connection
 mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
 db = MongoClient(mongo_uri)['test_db']
 
 class TodoListView(APIView):
 
     def get(self, request):
-        # Implement this method - return all todo items from db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        todos = db.todos.find()  
+        todo_list = [{"id": str(todo["_id"]), "description": todo["description"]} for todo in todos]
+        return Response(todo_list, status=status.HTTP_200_OK)
         
     def post(self, request):
-        # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        description = request.data.get("description", "")
+        if not description:
+            return Response({"error": "Description is required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        new_todo = {"description": description}
+        result = db.todos.insert_one(new_todo)
+        new_todo["_id"] = str(result.inserted_id)  
+        
+        return Response({"message": "TODO created successfully", "todo": new_todo}, status=status.HTTP_201_CREATED)
